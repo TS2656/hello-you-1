506c5644095366bfcae8a9d28e777f6c01098954
TS2656 <70953238+TS2656@users.noreply.github.com>
2020-09-14 10:33:46 +0200

    Add files via upload

---
+++ Hello-You.py
@@ -0,0 +1,13 @@
+Last login: Mon Sep 14 10:11:01 on ttys000
+ts2656@MacBook-Pro-van-Thijn ~ % python3
+Python 3.8.5 (v3.8.5:580fbb018f, Jul 20 2020, 12:11:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> print("Hello You!, ik ben Thijn")
+Hello You!, ik ben Thijn
+>>> print("Wie ben jij")
+Wie ben jij
+>>> print("Ik ben Timo")
+Ik ben Timo
+>>> print("Hello Timo")
+Hello Timo
